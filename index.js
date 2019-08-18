2aaf6d9697ae57be9d829f0e971c34e27341a807
hilarious-programmer-2558 <combatdstar@gmail.com>
2019-08-18 23:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function destructivelyRemoveFirstKitten() {
   return kittens;
 }
 
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
 function prependKitten(name) {
   return [name, ...kittens];
 }
